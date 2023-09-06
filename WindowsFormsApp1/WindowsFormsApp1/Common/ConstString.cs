166ee3840c40688aa43d4094d5ce3bbf701da322
佐藤 優真 <y.sato@polaris-corp.co.jp>
2023-09-06 10:47:56 +0900

    定数名が命名規則に従っていなかったため修正

--- WindowsFormsApp1/WindowsFormsApp1/Common/ConstString.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Common/ConstString.cs
@@ -11,9 +11,9 @@ namespace WindowsFormsApp1.Common
         public const string ConnectionString = "Server=localhost;User ID=root;Password=1105;Database=login_test";
         public const string EmptyMessage = "IDかPasswordが空です";
         public const string NotIdNumberMessage = "数字を入力してください";
-        public const string NotusersMessage = "該当するユーザーが見当たりません";
+        public const string NotUsersMessage = "該当するユーザーが見当たりません";
         public const string NotPwdMatchMessage = "パスワードが間違っています";
-        public const string LoginMessagE = "ログインしました";
+        public const string LoginMessage = "ログインしました";
         public const string LoginImpossible = "あと{0}ログイン出来ません";
         public const string ErrorMessage = "エラーです";
     }
